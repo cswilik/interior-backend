37ab274b67a47721bca95c1b9c645f6d1e78fc4b
CSWILIK <cswilik@gmail.com>
2021-02-09 12:39:47 -0500

    just commiting

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
   resources :parks
   resources :users
   post "/login", to: "auth#login"
+  get 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
