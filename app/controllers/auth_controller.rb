aec341afea606b338ab73ec0078b9712149775bb
CSWILIK <cswilik@gmail.com>
2021-03-10 12:26:41 -0500

    commiting

--- app/controllers/auth_controller.rb
+++ app/controllers/auth_controller.rb
@@ -7,7 +7,7 @@ class AuthController < ApplicationController
         token = JWT.encode({user_id: user.id}, secret, "HS256")
         render json: {user: UserSerializer.new(user), token: token}
       else 
-        render json: {error: "invalid email or password"}
+        render json: {error: "invalid email or password"}, status: :unauthorized
       end 
     end
 
